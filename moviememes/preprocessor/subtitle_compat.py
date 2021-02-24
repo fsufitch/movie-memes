ce36cdae20bdcda30a05010fddbd08d6465365b7
Filip Sufitchi <fsufitchi@gmail.com>
2021-02-24 18:59:13 -0500

    Add mimetype detection to SRT reading

--- moviememes/preprocessor/subtitle_compat.py
+++ moviememes/preprocessor/subtitle_compat.py
@@ -1,6 +1,8 @@
 """ Compatibility with, and abstraction from subtitle files """
 import datetime
 from typing import List
+
+import chardet
 from pysubparser import parser as subparser
 
 from moviememes.preprocessor.structs import Subtitle
@@ -8,9 +10,12 @@ from moviememes.preprocessor.structs import Subtitle
 
 def parse_subtitles(subtitle_file: str) -> List[Subtitle]:
     """ Return a list of Subtitle objects derived from the given file """
+    with open(subtitle_file, 'rb') as f:
+        chardet_result = chardet.detect(f.read())
+
     return [
         Subtitle(s.index, s.text, _to_sec(s.start), _to_sec(s.end))
-        for s in subparser.parse(subtitle_file)
+        for s in subparser.parse(subtitle_file, encoding=chardet_result['encoding'])
     ]
 
 
